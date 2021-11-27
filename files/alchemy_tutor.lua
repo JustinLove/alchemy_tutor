002be713eff1bfeb9bf765de5493554cea91195b
Justin Love <git@JustinLove.name>
2021-11-27 10:10:47 -0600

    extract formula file

--- files/alchemy_tutor.lua
+++ files/alchemy_tutor.lua
@@ -30,76 +30,16 @@ local function empty_container_of_materials(idx)
 end
 
 function at_pick_lab_set( x, y )
-	local formula_list = {
-		{
-			name = "hastium",
-			material1 = "magic_liquid_movement_faster",
-			material2 = "magic_liquid_faster_levitation",
-			output = "magic_liquid_faster_levitation_and_movement",
-		},
-		{
-			material1 = "radioactive_liquid",
-			material2 = "powder_empty",
-			cauldron_contents = "sand",
-			cauldron_material = "air",
-			other = at_planterbox,
-			output = "fungi_creeping",
-		},
-		{
-			name = "silver1",
-			material1 = "magic_liquid_random_polymorph",
-			material2 = "diamond",
-			output = "silver",
-		},
-		{
-			name = "silver2",
-			material1 = "magic_liquid_random_polymorph",
-			material2 = "gold",
-			output = "silver",
-		},
-		{
-			name = "silver3",
-			material1 = "copper",
-			material2 = "brass",
-			cauldron_contents = "water",
-			output = "silver",
-		},
-		{
-			name = "levi",
-			material1 = "brass",
-			material2 = "material_confusion",
-			output = "magic_liquid_faster_levitation",
-		},
-		{
-			material1 = "brass",
-			material2 = "diamond",
-			output = "purifying_powder",
-		},
-		{
-			name = "void1",
-			material1 = "radioactive_liquid",
-			material2 = "blood_worm",
-			cauldron_contents = "fungi",
-			output = "void_liquid",
-		},
-		{
-			name = "void2",
-			material1 = "diamond",
-			material2 = "magic_liquid_random_polymorph",
-			cauldron_contents = "radioactive_liquid",
-			output = "void_liquid",
-		},
-	}
 	local formulas = {}
-	for i,v in ipairs(formula_list) do
+	for i,v in ipairs(at_formula_list) do
 		formulas[v.name or v.output] = v
 	end
 	SetRandomSeed( x, y )
 	local d = math.sqrt(x*x + y*y)
 	local r = Random()
 	local f = r ^ (12000 / d)
-	local i = math.floor( f * #formula_list + 1 )
-	return formula_list[i]
+	local i = math.floor( f * #at_formula_list + 1 )
+	return at_formula_list[i]
 	--return formulas.hastium
 	--return formulas.fungi_creeping
 	--return formulas.silver3
@@ -207,3 +147,5 @@ function shuffleTable( t )
 		t[i], t[j] = t[j], t[i]
 	end
 end
+
+dofile_once(at_mod_path .. "/formula_list.lua")
---
+++ files/formula_list.lua
@@ -0,0 +1,60 @@
+at_formula_list = {
+	{
+		name = "hastium",
+		material1 = "magic_liquid_movement_faster",
+		material2 = "magic_liquid_faster_levitation",
+		output = "magic_liquid_faster_levitation_and_movement",
+	},
+	{
+		material1 = "radioactive_liquid",
+		material2 = "powder_empty",
+		cauldron_contents = "sand",
+		cauldron_material = "air",
+		other = at_planterbox,
+		output = "fungi_creeping",
+	},
+	{
+		name = "silver1",
+		material1 = "magic_liquid_random_polymorph",
+		material2 = "diamond",
+		output = "silver",
+	},
+	{
+		name = "silver2",
+		material1 = "magic_liquid_random_polymorph",
+		material2 = "gold",
+		output = "silver",
+	},
+	{
+		name = "silver3",
+		material1 = "copper",
+		material2 = "brass",
+		cauldron_contents = "water",
+		output = "silver",
+	},
+	{
+		name = "levi",
+		material1 = "brass",
+		material2 = "material_confusion",
+		output = "magic_liquid_faster_levitation",
+	},
+	{
+		material1 = "brass",
+		material2 = "diamond",
+		output = "purifying_powder",
+	},
+	{
+		name = "void1",
+		material1 = "radioactive_liquid",
+		material2 = "blood_worm",
+		cauldron_contents = "fungi",
+		output = "void_liquid",
+	},
+	{
+		name = "void2",
+		material1 = "diamond",
+		material2 = "magic_liquid_random_polymorph",
+		cauldron_contents = "radioactive_liquid",
+		output = "void_liquid",
+	},
+}
