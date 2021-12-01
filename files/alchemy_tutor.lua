36b2bd8a05856e561f09a4c45868653468c17f33
Justin Love <git@JustinLove.name>
2021-12-01 08:12:33 -0600

    forgetting to disable forced formula

--- files/alchemy_tutor.lua
+++ files/alchemy_tutor.lua
@@ -2,9 +2,9 @@ local at_mod_path = "mods/alchemy_tutor/files"
 
 --1439153766
 --1496269479
-at_test_formula = 'melt_steel'
 at_test_x = -200
 at_test_y = -100
+--at_test_formula = 'melt_steel'
 --at_test_player = true
 --at_test_lab = true
 
