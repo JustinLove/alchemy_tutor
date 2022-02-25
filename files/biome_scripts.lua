fac711a8f781ae58ccf359eadbf85f0c80f68d11
Justin Love <git@JustinLove.name>
2022-02-25 16:46:45 -0600

    remote lab chest in liquidcave

--- files/biome_scripts.lua
+++ files/biome_scripts.lua
@@ -4,19 +4,20 @@ if at_lab_chance == nil then
 end
 --at_lab_chance = 9999999
 
-local at_key_chance = math.floor(math.sqrt(at_lab_chance) * 10)
-local at_remote_lab_key = "mods/alchemy_tutor/files/entities/remote_lab_chest.xml"
+at_chest_chance = math.floor(math.sqrt(at_lab_chance) * 10)
+at_remote_lab_chest = "mods/alchemy_tutor/files/entities/remote_lab_chest.xml"
 
 function at_spawn_remote_lab_key( x, y )
 end
 
 at_base_spawn_heart = spawn_heart
+
 function spawn_heart( x, y )
 	-- same seeded call as base spawn heart; we should get the same number
 	local r = ProceduralRandom( x, y )
 
-	if 0.3 - at_key_chance < r and r < 0.3 + at_key_chance then
-		EntityLoad( at_remote_lab_key, x, y )
+	if 0.3 - at_chest_chance < r and r < 0.3 + at_chest_chance then
+		EntityLoad( at_remote_lab_chest, x, y )
 	else
 		at_base_spawn_heart( x, y )
 	end
--- files/biomes/liquidcave.lua
+++ files/biomes/liquidcave.lua
@@ -1,13 +1,11 @@
-local at_base_spawn_potion_altar = spawn_potion_altar
-
-dofile_once("mods/alchemy_tutor/files/remote_lab_spawns.lua")
+at_base_spawn_potion_altar = spawn_potion_altar
 
 function spawn_potion_altar(x, y)
 	local r = ProceduralRandom( x, y )
 	
 	if (r > 0.65) then
 		at_base_spawn_potion_altar( x, y )
-	elseif (r > 0.30) then
-		at_spawn_meditation_altar( x, y )
+	elseif (r > 0.65 - at_chest_chance) then
+		EntityLoad( at_remote_lab_chest, x, y )
 	end
 end
