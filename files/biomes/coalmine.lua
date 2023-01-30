bced80cf0515be535306023818f72ab8415553b9
Justin Love <git@JustinLove.name>
2023-01-29 19:57:52 -0600

    coalmine v visual

--- files/biomes/coalmine.lua
+++ files/biomes/coalmine.lua
@@ -6,7 +6,7 @@ dofile_once("mods/alchemy_tutor/files/spawns.lua")
 table.insert( g_pixel_scene_01, {
 		prob   			= at_lab_chance,
 		material_file 	= "mods/alchemy_tutor/files/biome_impl/coalmine_lab_v.png",
-		visual_file		= "",--"mods/alchemy_tutor/files/biome_impl/coalmine_lab_visual.png",
+		visual_file		= "mods/alchemy_tutor/files/biome_impl/coalmine_lab_v_visual.png",
 		background_file	= "",
 		is_unique		= 0
 	})
