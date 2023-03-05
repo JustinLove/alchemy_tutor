da1e9ed8e737ab8488021bd0ceeff5c1fd13b1c3
Justin Love <git@JustinLove.name>
2023-03-05 14:56:31 -0600

    rainforest h background

--- files/biomes/rainforest.lua
+++ files/biomes/rainforest.lua
@@ -3,7 +3,7 @@ dofile_once("mods/alchemy_tutor/files/spawns.lua")
 table.insert( g_pixel_scene_01, {
 		prob   			= at_lab_chance,
 		material_file 	= "mods/alchemy_tutor/files/biome_impl/rainforest_lab_v.png",
-		visual_file		= "",--"mods/alchemy_tutor/files/biome_impl/coalmine_lab_visual.png",
+		visual_file		= "",
 		background_file	= "",
 		is_unique		= 0
 	})
@@ -11,7 +11,7 @@ table.insert( g_pixel_scene_01, {
 table.insert( g_pixel_scene_02, {
 		prob   			= at_lab_chance,
 		material_file 	= "mods/alchemy_tutor/files/biome_impl/rainforest_lab_h.png",
-		visual_file		= "", --"mods/alchemy_tutor/files/biome_impl/coalmine_lab_visual.png",
-		background_file	= "",
+		visual_file		= "",
+		background_file	= "mods/alchemy_tutor/files/biome_impl/rainforest_lab_h_background.png",
 		is_unique		= 0
 	})
--- files/biomes/rainforest_dark.lua
+++ files/biomes/rainforest_dark.lua
@@ -3,7 +3,7 @@ dofile_once("mods/alchemy_tutor/files/spawns.lua")
 table.insert( g_pixel_scene_01, {
 		prob   			= at_lab_chance,
 		material_file 	= "mods/alchemy_tutor/files/biome_impl/rainforest_lab_v.png",
-		visual_file		= "",--"mods/alchemy_tutor/files/biome_impl/coalmine_lab_visual.png",
+		visual_file		= "",
 		background_file	= "",
 		is_unique		= 0,
 		color_material = { ["708040"] = { "soil_dark" } }
@@ -12,8 +12,8 @@ table.insert( g_pixel_scene_01, {
 table.insert( g_pixel_scene_02, {
 		prob   			= at_lab_chance,
 		material_file 	= "mods/alchemy_tutor/files/biome_impl/rainforest_lab_h.png",
-		visual_file		= "", --"mods/alchemy_tutor/files/biome_impl/coalmine_lab_visual.png",
-		background_file	= "",
+		visual_file		= "",
+		background_file	= "mods/alchemy_tutor/files/biome_impl/rainforest_lab_h_background.png",
 		is_unique		= 0,
 		color_material = { ["708040"] = { "soil_dark" } }
 	})
