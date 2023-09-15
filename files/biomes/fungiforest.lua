79456fcbe2e2eb456c3040861b678daf47028766
Justin Love <git@JustinLove.name>
2023-09-14 20:37:41 -0500

    overgrowth lab art

--- files/biomes/fungiforest.lua
+++ files/biomes/fungiforest.lua
@@ -4,7 +4,12 @@ at_biome_banned_materials = { "void_liquid", "blood_worm" }
 
 function at_spawn_mini_lab( x, y )
 	at_preclear_for_mini( x, y, 80 )
-	LoadPixelScene( "mods/alchemy_tutor/files/biome_impl/fungiforest_lab_mini.png", "", x-100, y-90, "", true )
+	LoadPixelScene(
+		"mods/alchemy_tutor/files/biome_impl/fungiforest_lab_mini.png",
+		"mods/alchemy_tutor/files/biome_impl/fungiforest_lab_mini_visual.png",
+		x-100, y-90,
+		"mods/alchemy_tutor/files/biome_impl/fungiforest_lab_mini_background.png",
+		true )
 end
 
 --spawn_heart = at_spawn_mini_lab
